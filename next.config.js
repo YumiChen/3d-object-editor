2367be2b4bee904f45fe642eb1ad376a1f1deb4d
Yumi Chen <27608365+YumiChen@users.noreply.github.com>
2023-11-23 17:28:34 -0400

    Update build config for Static Exports

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = {
+    output: 'export',
+    assetPrefix: isProd ? 'https://cdn.jsdelivr.net/gh/YumiChen/3d-object-editor@gh-pages' : undefined,
+}
 
 module.exports = nextConfig
